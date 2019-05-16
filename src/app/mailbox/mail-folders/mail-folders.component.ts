66b9abf31f5b70f7da18e5a6dcae43faae3e0903
g.demarco <devnull@localhost>
2019-05-16 17:49:57 +0200

    creazione e rinomina cartelle

--- src/app/mailbox/mail-folders/mail-folders.component.ts
+++ src/app/mailbox/mail-folders/mail-folders.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, ViewEncapsulation, Output, EventEmitter, OnDestroy, ViewChild } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation, Output, EventEmitter, OnDestroy, ViewChild, Input, ElementRef, ViewChildren } from "@angular/core";
 import { FiltersAndSorts, SortDefinition, SORT_MODES, AdditionalDataDefinition, FilterDefinition } from "@nfa/next-sdr";
-import { Pec, ENTITIES_STRUCTURE } from "@bds/ng-internauta-model";
+import { Pec, ENTITIES_STRUCTURE, Folder, FolderType } from "@bds/ng-internauta-model";
 import { PecService } from "src/app/services/pec.service";
 import { TreeNode, MenuItem } from "primeng/api";
 import { MailFoldersService, PecFolder, PecFolderType } from "./mail-folders.service";
 import { ToolBarService } from "../toolbar/toolbar.service";
 import { Subscription } from "rxjs";
-import { ContextMenu } from "primeng/primeng";
+import { ContextMenu, Tree } from "primeng/primeng";
+import { FolderService } from "src/app/services/folder.service";
 
 @Component({
   selector: "app-mail-folders",
@@ -23,42 +24,18 @@ export class MailFoldersComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription[] = [];
 
   @ViewChild("cm") public cm: ContextMenu;
+  @ViewChild("tree") public tree: Tree;
+  @ViewChildren("folderInput") public folderInput: ElementRef[];
+
   // @ViewChild("manageFolderPanel") public manageFolderPanel: OverlayPanel;
-
-  public showPecContextMenu = true;
-  public folderDialogProperties = {
-    title: "",
-    visible: false,
-    content: ""
-  };
-
-  public aaa: TreeNode[] = [
-    {
-      label: "cartella",
-      data: {
-        disabled: false
-      },
-      styleClass: "dis"
-    },  {
-      label: "cartella",
-      data: {
-        disabled: true
-      },
-      styleClass: "prova"
-    },  {
-      label: "cartella",
-      data: {
-        disabled: true
-      },
-      styleClass: "prova"
-    }
-  ];
-
 
   public mailfolders: MyTreeNode[] = [];
   // @Output("folderEmitter") private folderEmitter: EventEmitter<number> = new EventEmitter();
 
   public selectedNode: MyTreeNode;
+  public previousSelectedNode: MyTreeNode;
+  private _abortSaveFolder: boolean = false;
+  private _pressedEnterOrEsc: boolean = false;
 
   public cmItems: MenuItem[];
   public pecCmItems: MenuItem[] = [
@@ -81,7 +58,9 @@ export class MailFoldersComponent implements OnInit, OnDestroy {
     },
   ];
 
-  constructor(private pecService: PecService,
+  constructor(
+    private pecService: PecService,
+    private folderService: FolderService,
     private mailFoldersService: MailFoldersService,
     private toolBarService: ToolBarService) { }
 
@@ -117,33 +96,25 @@ export class MailFoldersComponent implements OnInit, OnDestroy {
     const children: MyTreeNode[] = [];
     if (pec.folderList) {
       for (const folder of pec.folderList) {
-        children.push({
-          "label": folder.description,
-          "data": {
-            type: PecFolderType.FOLDER,
-            data: folder
-          } as PecFolder,
-          // "type": PecTreeNodeType.FOLDER,
-          "expandedIcon": "pi pi-folder-open",
-          "collapsedIcon": "pi pi-folder",
-          "styleClass": "tree-node-style",
-          "editable": false
-        });
+        const p: Pec = new Pec();
+        p.id = pec.id;
+        folder.idPec = p;
+        children.push(this.buildFolderNode(folder));
       }
     }
     return {
-      "label": pec.indirizzo,
-      "data": {
+      label: pec.indirizzo,
+      data: {
         type: PecFolderType.PEC,
         data: pec
       } as PecFolder,
-      // "type": PecTreeNodeType.PEC,
-      "expandedIcon": "pi pi-folder-open",
-      "collapsedIcon": "pi pi-folder",
-      "children": children,
-      "selectable": true,
-      "styleClass": MailFoldersComponent.ROOT_NODE_NOT_SELECTED_STYLE_CLASS,
-      "editable": false
+      expandedIcon: "pi pi-folder-open",
+      collapsedIcon: "pi pi-folder",
+      children: children,
+      selectable: true,
+      styleClass: MailFoldersComponent.ROOT_NODE_NOT_SELECTED_STYLE_CLASS,
+      editable: false,
+      key: PecFolderType.PEC + "_" + pec.id
     } as MyTreeNode;
   }
 
@@ -153,34 +124,87 @@ export class MailFoldersComponent implements OnInit, OnDestroy {
       const menuItemSelected: MenuItem = event.item;
       switch (menuItemSelected.id) {
         case "NewFolder":
-        // this.folderDialogProperties.show(this.manageFolderPanelPropertiers);
-        // this.folderDialogProperties.title = menuItemSelected.label;
-        // this.folderDialogProperties.visible = true;
-          this.mailfolders[1].children.push({
-            label: "Nuova Cartella",
-            editable: true,
-            expandedIcon: "pi pi-folder-open",
-            collapsedIcon: "pi pi-folder",
-          });
-          console.log("new folder", this.selectedNode);
+          // console.log("new folder", this.selectedNode);
+          const folderToInsert = this.buildCustomFolder(this.selectedNode.data.data as Pec, "Nuova Cartella", true);
+          this.selectedNode.children.push(this.buildFolderNode(folderToInsert, true));
+          setTimeout(() => {
+            const a = this.folderInput;
+            a.find(e => e.nativeElement.id === this.selectedNode.key).nativeElement.focus();
+          }, 0);
+          this.selectedNode.expanded = true;
+          this.selectedNode = this.selectedNode.children[this.selectedNode.children.length - 1];
         break;
         case "RenameFolder":
           console.log("rename folder", this.selectedNode);
+          const folderToRename: Folder = this.selectedNode.data.data as Folder;
+          if (folderToRename.type === FolderType.CUSTOM) {
+            this.selectedNode.editable = true;
+            setTimeout(() => {
+              this.folderInput.find(e => e.nativeElement.id === this.selectedNode.key).nativeElement.focus();
+            }, 0);
+          } else {
+
+          }
         break;
       }
     }
   }
 
+  private buildCustomFolder(pecContainer: Pec, name?: string, pushInPecContainer?: boolean): Folder {
+    const folder: Folder = new Folder();
+    if (name) {
+      name = name.trim();
+      folder.description = name;
+      folder.name = name.replace(/\s+/, "_").toLowerCase();
+    }
+    folder.type = FolderType.CUSTOM;
+    const pec: Pec = new Pec();
+    pec.id = pecContainer.id;
+    folder.idPec = pec;
+    if (!!pushInPecContainer) {
+      pecContainer.folderList.push(folder);
+    }
+    return folder;
+  }
+
+  private buildFolderNode(folder: Folder, editable: boolean = false): MyTreeNode {
+    return {
+      label: folder.description,
+      data: {
+        type: PecFolderType.FOLDER,
+        data: folder
+      } as PecFolder,
+      expandedIcon: "pi pi-folder-open",
+      collapsedIcon: "pi pi-folder",
+      styleClass: "tree-node-style",
+      editable: editable,
+      key: PecFolderType.FOLDER + "_" + (folder.id ? folder.id : "new")
+    };
+  }
+
   public handleNodeSelect(name: string, event: any) {
     switch (name) {
     case "onContextMenuSelect":
+      if (this.mailfolders) {
+        this.mailfolders.map(m => m.styleClass = MailFoldersComponent.ROOT_NODE_NOT_SELECTED_STYLE_CLASS);
+        this.selectRootNode(event.node, true);
+        this.mailFoldersService.selectedPecFolder(event.node.data);
+      }
       if (event.node && event.node.data && (event.node as MyTreeNode).data.type === PecFolderType.PEC) {
         this.cmItems = this.pecCmItems;
       } else {
         this.cmItems = this.folderCmItems;
+        const renameItems = this.cmItems.find(item => item.id === "RenameFolder");
+        if (renameItems && (event.node.data.data as Folder).type !== FolderType.CUSTOM ) {
+          renameItems.disabled = true;
+        } else {
+          renameItems.disabled = false;
+        }
       }
     break;
-    // case "selectionChange":
+    case "onNodeUnselect":
+      this.previousSelectedNode = this.selectedNode;
+    break;
     //   if ((event as TreeNode).type === PecTreeNodeType.PEC) {
     //     this.showPecContextMenu = true;
     //   } else {
@@ -197,9 +221,81 @@ export class MailFoldersComponent implements OnInit, OnDestroy {
     }
   }
 
-  // private deselectAllNode() {
+  public onLostFocus(event, value?: string) {
+    if (this._pressedEnterOrEsc) {
+      this._pressedEnterOrEsc = false;
+    } else {
+      this.saveFolder(value);
+    }
+  }
+  public onEnterPressed(event, value?: string) {
+    this._pressedEnterOrEsc = true;
+    this.saveFolder(value);
+  }
 
-  // }
+  public onEscPressed(event, value?: string) {
+    this._pressedEnterOrEsc = false;
+    this.abortSaveFolder();
+  }
+
+  public saveFolder(name?: string) {
+    console.log("SAVE event", name);
+    if (!this._abortSaveFolder) {
+      const folder: Folder = this.selectedNode.data.data as Folder;
+      const inserting: boolean = !folder.id ? true : false;
+      if (!name) {
+        name = folder.description;
+      }
+      if (name) {
+        name = name.trim();
+        folder.description = name;
+        this.selectedNode.label = name;
+        folder.name = name.replace(/\s+/gm, "_").toLowerCase();
+
+        if (this.validateName(name)) {
+          this.selectedNode.editable = false;
+          if (!inserting) {
+            this.folderService.patchHttpCall(folder, folder.id).subscribe((f: Folder) => {
+              this.selectedNode.data.data = f;
+              this.mailFoldersService.selectedPecFolder(this.selectedNode.data);
+              console.log(this.selectedNode.data.data);
+            });
+          } else {
+            this.folderService.postHttpCall(folder).subscribe((f: Folder) => {
+              this.selectedNode.data.data = f;
+              this.selectedNode.key = PecFolderType.FOLDER + "_" + f.id;
+              this.mailFoldersService.selectedPecFolder(this.selectedNode.data);
+              console.log(this.selectedNode.data.data);
+            });
+          }
+        }
+      } else {
+        this.abortSaveFolder();
+      }
+      console.log(this.selectedNode.data.data);
+    }
+    this._abortSaveFolder = false;
+  }
+
+  public validateName(name: string) {
+    // this.selectedNode.parent.children.some
+    return true;
+  }
+
+  public abortSaveFolder() {
+    console.log("ABORT");
+    this._abortSaveFolder = true;
+    const folder: Folder = this.selectedNode.data.data as Folder;
+    const inserting: boolean = !folder.id ? true : false;
+    console.log("inserting", inserting);
+    if (inserting) {
+      this.selectedNode.parent.children.pop();
+      this.selectedNode = this.previousSelectedNode;
+    } else {
+      this.selectedNode.label = folder.description;
+      this.selectedNode.editable = false;
+    }
+  }
 
   private selectRootNode(node: MyTreeNode, changeOnlyStyle: boolean) {
     if (node.data.type === PecFolderType.PEC) {
