e7643d0601b9029f74c3566d6fceb4b8d3d99cf7
g.demarco <devnull@localhost>
2019-06-18 16:47:28 +0200

    fix per ricaricare impostazioni nel caso di apertura da scrivania

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -19,11 +19,15 @@ export class SettingsService {
         // if (!this.loggedUser || utente.getUtente().id !== this.loggedUser.getUtente().id) {
           this.loggedUser = utente;
           if (this.loggedUser.getImpostazioniApplicazione()) {
-            this.impostazioniVisualizzazione = JSON.parse(this.loggedUser.getImpostazioniApplicazione().impostazioniVisualizzazione);
+            const impostazioniVisualizzazioneLoggedUser = JSON.parse(this.loggedUser.getImpostazioniApplicazione().impostazioniVisualizzazione);
+            const impostazioniVisualizzazioneNewUtente = JSON.parse(utente.getImpostazioniApplicazione().impostazioniVisualizzazione);
+            if (!this.compareObjects(impostazioniVisualizzazioneLoggedUser, impostazioniVisualizzazioneNewUtente)) {
+              this.impostazioniVisualizzazione = impostazioniVisualizzazioneNewUtente;
+              this.doNotify(this.impostazioniVisualizzazione);
+            }
           } else {
             this.impostazioniVisualizzazione = {};
           }
-          this.doNotify(this.impostazioniVisualizzazione);
         // } else {
         //   this.loggedUser = utente;
         // }
@@ -57,6 +61,42 @@ export class SettingsService {
   */
   doNotify(settings: any) {
     this.settingsChangedNotifier$.next(settings);
+  }
+
+  /**
+     * This nasty bugger finds out whether the objects are _traditionally_ equal to each other, like you might assume someone else would have put this function in vanilla JS already
+     * One thing to note is that it uses coercive comparison (==) on properties which both objects have, not strict comparison (===)
+     * @param base The base object which you would like to compare another object to
+     * @param compare The object to compare to base
+     * @returns boolean indicating whether or not the objects have all the same properties and those properties are ==
+     */
+    private compareObjects(base: any, compare: any): boolean {
+
+      // loop through all properties in base object
+      for (const baseProperty in base) {
+
+          // determine if comparrison object has that property, if not: return false
+          if (compare.hasOwnProperty(baseProperty)) {
+              switch (typeof base[baseProperty]) {
+                  // if one is object and other is not: return false
+                  // if they are both objects, recursively call this comparison function
+                  case "object":
+                      if ( typeof compare[baseProperty] !== "object" || !this.compareObjects(base[baseProperty], compare[baseProperty]) ) { return false; } break;
+                  // if one is function and other is not: return false
+                  // if both are functions, compare function.toString() results
+                  case "function":
+                      if ( typeof compare[baseProperty] !== "function" || base[baseProperty].toString() !== compare[baseProperty].toString() ) { return false; } break;
+                  // otherwise, see if they are equal using coercive comparison
+                  default:
+                      if ( base[baseProperty] != compare[baseProperty] ) { return false; }
+              }
+          } else {
+              return false;
+          }
+      }
+
+      // returns true only after false HAS NOT BEEN returned through all loops
+      return true;
   }
 }
 
