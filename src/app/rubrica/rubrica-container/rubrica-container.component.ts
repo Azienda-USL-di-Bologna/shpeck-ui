2e05a0167dd1b26fdde9e69b5b6cf62656d0afc1
c.toparcean <devnull@localhost>
2020-10-23 09:34:21 +0200

    create new component rubrica container

---
+++ src/app/rubrica/rubrica-container/rubrica-container.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Output, EventEmitter, ViewChild } from "@angular/core";
+import { Router, ActivatedRoute, RouterOutlet, ActivationStart } from "@angular/router";
+
+@Component({
+  selector: "app-rubrica-container",
+  templateUrl: "./rubrica-container.component.html",
+  styleUrls: ["./rubrica-container.component.scss"]
+})
+export class RubricaContainerComponent implements OnInit {
+
+  @Output() closeRubricaPopup = new EventEmitter<any>();
+  // @ViewChild(RouterOutlet, {static: false}) outlet: RouterOutlet;
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    // this.router.events.subscribe(e => {
+    //   if (e instanceof ActivationStart && e.snapshot.outlet === "administration") this.outlet.deactivate();
+    // });
+
+    console.log("RubricaContainerComponent route url: ", this.route.url);
+    // this.router.navigate(["", { outlets: { rubricaPopup: "rubrica" }}], {
+    //   queryParams: {
+    //     mode: "selection",
+    //     from: "pec"
+    //   }
+    // });
+  }
+
+  public onClose() {
+    console.log("close popup");
+    this.closeRubricaPopup.emit({});
+  }
+
+}
