84a1292474324ab76c07cb47ef4785dcf3440aaa
g.gianluca <g.guidi@nsi.it>
2022-08-11 17:46:42 +0200

    log nel dettaglio

--- src/app/mailbox/mail-detail/mail-detail.module.ts
+++ src/app/mailbox/mail-detail/mail-detail.module.ts
@@ -5,11 +5,13 @@ import { PrimengModule } from 'src/app/primeng.module';
 import { FormsModule } from '@angular/forms';
 import { SanitizeHtmlPipe } from 'src/app/utils/sanitize-html-pipe';
 import { RecepitsComponent } from '../recepits/recepits.component';
+import { CommonComponentsModule } from "@bds/common-components";
+
 
 @NgModule({
     declarations: [MailDetailComponent, SanitizeHtmlPipe, RecepitsComponent,],
     imports: [
-      CommonModule, PrimengModule, FormsModule
+      CommonModule, PrimengModule, FormsModule, CommonComponentsModule
     ],
     providers: [],
     exports: [MailDetailComponent, SanitizeHtmlPipe, RecepitsComponent,]
