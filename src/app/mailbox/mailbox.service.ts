d4ac7be61dec0836960b3967f141a5e0703eb886
DESKTOP-2L12LJ5\Tommaso <t.ravaioli@nextsw.it>
2023-12-11 10:07:46 +0100

    RM domicilio digitale PEC

--- src/app/mailbox/mailbox.service.ts
+++ src/app/mailbox/mailbox.service.ts
@@ -3,42 +3,49 @@ import { BehaviorSubject, Observable, Subject } from "rxjs";
 import { PecFolderType, PecFolder } from "./mail-folders/mail-folders.service";
 
 @Injectable({
-  providedIn: "root"
+  providedIn: "root",
 })
 export class MailboxService {
-    private _sorting: BehaviorSubject<Sorting> = new BehaviorSubject<Sorting>(null);
-    private _totalMessageNumberDescriptor$: Subject<TotalMessageNumberDescriptor> = new Subject<TotalMessageNumberDescriptor>();
+  private _sorting: BehaviorSubject<Sorting> = new BehaviorSubject<Sorting>(
+    null
+  );
+  private _totalMessageNumberDescriptor$: Subject<TotalMessageNumberDescriptor> =
+    new Subject<TotalMessageNumberDescriptor>();
 
-    public get sorting(): Observable<Sorting> {
-        return this._sorting.asObservable();
-    }
+  public get sorting(): Observable<Sorting> {
+    return this._sorting.asObservable();
+  }
 
-    public setSorting(sorting: Sorting): void {
-        this._sorting.next(sorting);
-    }
+  public setSorting(sorting: Sorting): void {
+    this._sorting.next(sorting);
+  }
 
-    public get totalMessageNumberDescriptor$(): Observable<TotalMessageNumberDescriptor> {
-        return this._totalMessageNumberDescriptor$.asObservable();
-    }
+  public get totalMessageNumberDescriptor$(): Observable<TotalMessageNumberDescriptor> {
+    return this._totalMessageNumberDescriptor$.asObservable();
+  }
 
-    public setTotalMessageNumberDescriptor(totalMessageNumberDescriptor: TotalMessageNumberDescriptor) {
-        this._totalMessageNumberDescriptor$.next(totalMessageNumberDescriptor);
-    }
+  public setTotalMessageNumberDescriptor(
+    totalMessageNumberDescriptor: TotalMessageNumberDescriptor
+  ) {
+    this._totalMessageNumberDescriptor$.next(totalMessageNumberDescriptor);
+  }
 }
 
 export interface TotalMessageNumberDescriptor {
-    messageNumber: number;
-    pecFolder: PecFolder;
+  messageNumber: number;
+  pecFolder: PecFolder;
 }
 
 export interface Sorting {
-    field: string;
-    sortMode: string;
+  field: string;
+  sortMode: string;
 }
 
 export class FilteredContactMultiple {
-    id: number;
-    descrizione: string;
-    dettaglioContatto: string;
-    tipo: string;
-  }
+  id: number;
+  descrizione: string;
+  descrizioneDettaglioContatto: string;
+  descrizioneContatto: string;
+  tipo: string;
+  domicilioDigitale: boolean;
+}
